9ee8e58ad285700946d4e660a35adde95a6e3fdc
brunopaim <bruwill53@gmail.com>
2023-09-11 08:04:51 -0300

    initial commit

---
+++ src/controllers/clients.js
@@ -0,0 +1,44 @@
+import ClientRepository from "../models/clientsModel.js";
+
+function findAll(req, res) {
+  ClientRepository.findAll().then((result) => res.json(result));
+}
+
+function findClient(req, res) {
+  ClientRepository.findByPk(req.params.id).then((result) => res.json(result));
+}
+
+function addClient(req, res) {
+  ClientRepository.create({
+    nome: req.body.nome,
+    email: req.body.email,
+  }).then((result) => res.json(result));
+}
+
+async function updateClient(req, res) {
+  await ClientRepository.update(
+    {
+      nome: req.body.nome,
+      email: req.body.email,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  );
+
+  ClientRepository.findByPk(req.params.id).then((result) => res.json(result));
+}
+
+async function deleteClient(req, res) {
+  await ClientRepository.destroy({
+    where: {
+      id: req.params.id,
+    },
+  });
+
+  ClientRepository.findAll().then((result) => res.json(result));
+}
+
+export default { findAll, addClient, findClient, updateClient, deleteClient };
